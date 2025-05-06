898cc42ab58b0c6b30718a6af00198331474a18e
Kazuma Sakaguchi <sakaguchi@KazumanoMacBook-Pro.local>
2025-05-07 07:59:35 +0900

    loggerリファクタ

--- core/logger/logger.go
+++ core/logger/logger.go
@@ -1,28 +1,9 @@
 package logger
 
-import (
-	"log/slog"
-	"os"
-)
-
-type Logger struct {
-	*slog.Logger
-}
-
-func New(env string) *Logger {
-	var handler slog.Handler
-
-	if env == "prod" {
-		handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-			Level: slog.LevelInfo,
-		})
-	} else {
-		handler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-			Level: slog.LevelDebug,
-		})
-	}
-
-	return &Logger{
-		slog.New(handler),
-	}
+type Logger interface {
+	Info(msg string)
+	Warn(msg string)
+	Error(msg string)
+	Debug(msg string)
+	With(key string, val any) Logger
 }
