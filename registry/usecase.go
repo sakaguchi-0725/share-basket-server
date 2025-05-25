f0d0d4bcd9347ec60fe14bf9bf9f4771581b5f67
Kazuma Sakaguchi <sakaguchi@KazumanoMacBook-Pro.local>
2025-05-25 22:45:54 +0900

    refactor: DIのinterfaceをやめてstructする

--- registry/usecase.go
+++ registry/usecase.go
@@ -4,120 +4,69 @@ import (
 	"sharebasket/usecase"
 )
 
-type (
-	UseCase interface {
-		NewLogin() usecase.Login
-		NewSignUp() usecase.SignUp
-		NewSignUpConfirm() usecase.SignUpConfirm
-		NewVerifyToken() usecase.VerifyToken
-		NewGetAccount() usecase.GetAccount
-		NewGetCategories() usecase.GetCategories
-		NewCreatePersonalItem() usecase.CreatePersonalItem
-		NewGetPersonalItems() usecase.GetPersonalItems
-		NewUpdatePersonalItem() usecase.UpdatePersonalItem
-		NewDeletePersonalItem() usecase.DeletePersonalItem
-		NewCreateFamily() usecase.CreateFamily
-		NewInvitationFamily() usecase.InvitationFamily
-		NewJoinFamily() usecase.JoinFamily
-		NewCreateFamilyItem() usecase.CreateFamilyItem
-	}
-
-	usecaseImpl struct {
-		repo    Repository
-		service Service
-	}
-)
-
-func (u *usecaseImpl) NewCreateFamilyItem() usecase.CreateFamilyItem {
-	return usecase.NewCreateFamilyItem(
-		u.repo.NewAccount(),
-		u.repo.NewFamily(),
-		u.repo.NewFamilyItem(),
-		u.service.NewFamily(),
-	)
+type UseCase struct {
+	Login              usecase.Login
+	SignUp             usecase.SignUp
+	SignUpConfirm      usecase.SignUpConfirm
+	VerifyToken        usecase.VerifyToken
+	GetAccount         usecase.GetAccount
+	GetCategories      usecase.GetCategories
+	CreatePersonalItem usecase.CreatePersonalItem
+	GetPersonalItems   usecase.GetPersonalItems
+	UpdatePersonalItem usecase.UpdatePersonalItem
+	DeletePersonalItem usecase.DeletePersonalItem
+	CreateFamily       usecase.CreateFamily
+	InvitationFamily   usecase.InvitationFamily
+	JoinFamily         usecase.JoinFamily
+	CreateFamilyItem   usecase.CreateFamilyItem
 }
 
-func (u *usecaseImpl) NewJoinFamily() usecase.JoinFamily {
-	return usecase.NewJoinFamily(u.repo.NewAccount(), u.repo.NewFamily())
-}
-
-func (u *usecaseImpl) NewInvitationFamily() usecase.InvitationFamily {
-	return usecase.NewInvitationFamily(
-		u.repo.NewAccount(),
-		u.repo.NewFamily(),
-		u.service.NewFamily(),
-	)
-}
-
-func (u *usecaseImpl) NewCreateFamily() usecase.CreateFamily {
-	return usecase.NewCreateFamily(
-		u.repo.NewAccount(),
-		u.repo.NewFamily(),
-		u.service.NewFamily(),
-	)
-}
-
-func (u *usecaseImpl) NewDeletePersonalItem() usecase.DeletePersonalItem {
-	return usecase.NewDeletePersonalItem(
-		u.repo.NewAccount(),
-		u.repo.NewPersonalItem(),
-	)
-}
-
-func (u *usecaseImpl) NewUpdatePersonalItem() usecase.UpdatePersonalItem {
-	return usecase.NewUpdatePersonalItem(
-		u.repo.NewAccount(),
-		u.repo.NewPersonalItem(),
-	)
-}
-
-func (u *usecaseImpl) NewGetPersonalItems() usecase.GetPersonalItems {
-	return usecase.NewGetPersonalItems(
-		u.repo.NewAccount(),
-		u.repo.NewPersonalItem(),
-	)
-}
-
-func (u *usecaseImpl) NewCreatePersonalItem() usecase.CreatePersonalItem {
-	return usecase.NewCreatePersonalItem(
-		u.repo.NewAccount(),
-		u.repo.NewPersonalItem(),
-	)
-}
-
-func (u *usecaseImpl) NewGetAccount() usecase.GetAccount {
-	return usecase.NewGetAccount(u.repo.NewAccount())
-}
-
-func (u *usecaseImpl) NewGetCategories() usecase.GetCategories {
-	return usecase.NewGetCategories(u.repo.NewCategory())
-}
-
-func (u *usecaseImpl) NewLogin() usecase.Login {
-	return usecase.NewLogin(u.repo.NewAuthenticator())
-}
-
-func (u *usecaseImpl) NewSignUp() usecase.SignUp {
-	return usecase.NewSignUp(
-		u.repo.NewAuthenticator(),
-		u.repo.NewUser(),
-		u.repo.NewAccount(),
-		u.service.NewUser(),
-		u.repo.NewTransaction(),
-	)
-}
-
-func (u *usecaseImpl) NewSignUpConfirm() usecase.SignUpConfirm {
-	return usecase.NewSignUpConfirm(u.repo.NewAuthenticator())
-}
-
-func (u *usecaseImpl) NewVerifyToken() usecase.VerifyToken {
-	return usecase.NewVerifyToken(u.repo.NewAuthenticator())
-}
-
-func NewUseCase(r Repository, s Service) UseCase {
-	return &usecaseImpl{
-		repo:    r,
-		service: s,
+func NewUseCase(r *Repository, s *Service) *UseCase {
+	return &UseCase{
+		Login: usecase.NewLogin(r.Authenticator),
+		SignUp: usecase.NewSignUp(
+			r.Authenticator,
+			r.User,
+			r.Account,
+			s.User,
+			r.Transaction,
+		),
+		SignUpConfirm: usecase.NewSignUpConfirm(r.Authenticator),
+		VerifyToken:   usecase.NewVerifyToken(r.Authenticator),
+		GetAccount:    usecase.NewGetAccount(r.Account),
+		GetCategories: usecase.NewGetCategories(r.Category),
+		CreatePersonalItem: usecase.NewCreatePersonalItem(
+			r.Account,
+			r.PersonalItem,
+		),
+		GetPersonalItems: usecase.NewGetPersonalItems(
+			r.Account,
+			r.PersonalItem,
+		),
+		UpdatePersonalItem: usecase.NewUpdatePersonalItem(
+			r.Account,
+			r.PersonalItem,
+		),
+		DeletePersonalItem: usecase.NewDeletePersonalItem(
+			r.Account,
+			r.PersonalItem,
+		),
+		CreateFamily: usecase.NewCreateFamily(
+			r.Account,
+			r.Family,
+			s.Family,
+		),
+		InvitationFamily: usecase.NewInvitationFamily(
+			r.Account,
+			r.Family,
+			s.Family,
+		),
+		JoinFamily: usecase.NewJoinFamily(r.Account, r.Family),
+		CreateFamilyItem: usecase.NewCreateFamilyItem(
+			r.Account,
+			r.Family,
+			r.FamilyItem,
+			s.Family,
+		),
 	}
 }
